d286d62702c3d22f53ff56992ca4db6fbf5192db
SvitlanaTimofeyeva <s.timofeyeva@dinamicka.com>
2016-11-18 07:43:54 -0800

    update

--- PetroLogistics/PetroLogistics/routes/index.js
+++ PetroLogistics/PetroLogistics/routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
-});
+}); 
+
+router.get('/about', function (req, res) {
+    res.render('about'); 
+})
 
 module.exports = router;
